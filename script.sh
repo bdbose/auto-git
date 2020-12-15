01ef1b0e11f2f029a936c59fa2e849a0e4dc58b6
bdbose <bdbose123@gmail.com>
2020-12-15 20:24:20 +0530

    fix : automation

--- script.sh
+++ script.sh
@@ -4,35 +4,33 @@ var=10
 ran=$(($RANDOM % $var))
 echo $ran
 
-set USER
-
 echo $USER
 
 htm="<html><head><title>Get Your Lucky Number</title><link rel='stylesheet' href='style.css'><meta name='viewport' content='width=device-width, initial-scale=1.0'></head><body><div class='main'><span>Today's Lucky Number :</span> $ran </div></body></html>"
 
 echo $htm
 
-# git pull
+git pull
 
-# cat << EOF > index.html 
-# $htm
-# EOF
+cat << EOF > index.html 
+$htm
+EOF
 
-# timestamp=$(date +%T)
+timestamp=$(date +%T)
 
-# cat << EOF > README.md
-# # This is a Auto Git Commit Script :robot:
-# ## :supervillain_man: Running on AWS automatically updating the webpage everyday
-# ## :earth_asia: Checkout Website:  https://bdbose.github.io/auto-git/index.html
+cat << EOF > README.md
+# This is a Auto Git Commit Script :robot:
+## :supervillain_man: Running on AWS automatically updating the webpage everyday
+## :earth_asia: Checkout Website:  https://bdbose.github.io/auto-git/index.html
 
 
-# - :alarm_clock: Last Updated at $timestamp
-# EOF
+- :alarm_clock: Last Updated at $timestamp
+EOF
 
-# git add .
+git add .
 
-# git commit -m "Index Updated"
+git commit -m "Index Updated"
 
-# git push
+git push
 
 cd ..
